7d45c7694019f8947ff280832413716276beaa5f
ARYAN C R <57360045+aryancrajan@users.noreply.github.com>
2023-09-01 12:00:56 +0530

    music links as objects

--- src/App.js
+++ src/App.js
@@ -1,47 +1,37 @@
-//import ReactPlayer from 'react-player'
 import './App.css';
 import Player from './player';
 import React, {useState} from 'react';
+import Music from './music';
 
 function App() {
   let [showComponent, setShowComponent] = useState(false);
+  const [selectedMusic, setSelectedMusic] = useState('');
   let [url,setUrl]=useState('');
+
   const handleViewClick=()=>{
     setShowComponent(true);
   };
+
   const handleChange = (event) => {
-    setUrl(event.target.value);
+    setSelectedMusic(event.target.value);
+    setUrl(Music[event.target.value]);
   }
   return (
     <div className="Video Player">
       <center><p>React Video Player</p>
        <br />
-
-       <label>
-        <h1>Music</h1><br />
-        <input
-          type="radio"
-          name="music"
-          value="https://youtu.be/Rz-hqTc1KXk?si=Q6Ax8Vllevhx8gYs"
-          onChange={handleChange}
-        /> Vettam
-      </label>
-      <label>
-        <input
-          type="radio"
-          name="music"
-          value="https://youtu.be/yTRHnroQZuM?si=N97uG5waBETPbL5K"
-          onChange={handleChange}
-        /> Ranam
-      </label>
-      <label>
-        <input
-          type="radio"
-          name="music"
-          value="https://youtu.be/6LD30ChPsSs?si=fYZX9_QLfpM9zRdJ"
-          onChange={handleChange}
-        /> Life of Ram
-      </label>
+       {Object.entries(Music).map(([musicName, musicUrl]) => (
+          <label key={musicName}>
+            <input
+              type="radio"
+              name="music"
+              value={musicName}
+              checked={selectedMusic === musicName}
+              onChange={handleChange}
+            /> {musicName}
+          </label>
+        ))}
+       
       <br /> <button onClick={handleViewClick}>View Video</button>
        {showComponent && <Player url= {url} />}
 
---
+++ src/music.jsx
@@ -0,0 +1,9 @@
+
+
+let Music={
+    vettam:"https://youtu.be/Rz-hqTc1KXk?si=Q6Ax8Vllevhx8gYs",
+    ranam:"https://youtu.be/yTRHnroQZuM?si=N97uG5waBETPbL5K",
+    lifeOfRam:"https://youtu.be/6LD30ChPsSs?si=fYZX9_QLfpM9zRdJ",
+  }
+
+  export default Music
